368e5c42b5dfa1d7f2d91f3a9c8eb6f5f23f2d42
whoinlee <whoinlee@gmail.com>
2017-03-06 11:01:55 -0500

    Added Minified HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
+	minifyHTML = require('gulp-minify-html'),
 	browserify = require('gulp-browserify');
 
 var env,
@@ -82,7 +83,7 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 });
 
@@ -94,10 +95,10 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-	gulp.src(htmlSources)
-		.pipe(
-			connect.reload()
-			)
+	gulp.src('builds/development/*.html')
+		.pipe( gulpif(env === 'production', minifyHTML()) )
+		.pipe( gulpif(env === 'production', gulp.dest(outputDir)) )
+		.pipe(connect.reload())
 });
 
 gulp.task('json', function() {
