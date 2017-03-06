ca1ce76e77aaf4320abbec05dd27eac98e7a7898
whoinlee <whoinlee@gmail.com>
2017-03-06 08:52:15 -0500

    Added Coffeescript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 //-- node js commands
 var gulp = require('gulp'),	
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log("Learning Gulp & Browserify Workflows");
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	// gutil.log("Learning Gulp & Browserify Workflows");
+	gulp.src(coffeeSources)
+		.pipe(
+			coffee({ bare:true })
+			.on('error', gutil.log)
+		)
+		.pipe(
+			gulp.dest('components/scripts')
+		)
 });
